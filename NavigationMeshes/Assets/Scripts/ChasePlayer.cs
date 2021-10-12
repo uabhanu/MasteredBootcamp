d7ada9ac6bfaf1ee23ff5143dc11621a8167da3f
Bhanu <uabhanu@gmail.com>
2021-10-12 21:34:45 +0100

    Mastered Bootcamp : Navigation Meshes Complete

---
+++ NavigationMeshes/Assets/Scripts/ChasePlayer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class ChasePlayer : MonoBehaviour {
+
+    GameObject player;
+    NavMeshAgent agent;
+
+    void Start() {
+
+        player = GameObject.FindWithTag("Player");
+        agent = this.GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        agent.SetDestination(player.transform.position);
+    }
+}
