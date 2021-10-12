25b38cd44b03fc54bf5e53b67bd32744492b3ed7
Bhanu <uabhanu@gmail.com>
2021-10-12 21:04:03 +0100

    Mastered Bootcamp : Navigation Meshes Chapter 3 Part 1 & 2 Complete

---
+++ NavigationMeshes/Assets/Scripts/AIControl.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class AIControl : MonoBehaviour {
+
+    // Storage for the prefab
+    public NavMeshAgent agent;
+
+    void Start() {
+
+        // Grab hold of the agents NavMeshAgent
+        agent = this.GetComponent<NavMeshAgent>();
+    }
+}
---
+++ NavigationMeshes/Assets/Scripts/AgentManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class AgentManager : MonoBehaviour {
+
+    // An array of GameObjects to store all the agents
+    GameObject[] agents;
+
+    void Start() {
+
+        // Grab everything with the 'ai' tag
+        agents = GameObject.FindGameObjectsWithTag("ai");
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        // Check for left mouse button
+        if (Input.GetMouseButtonDown(0)) {
+
+            // Store the hit info
+            RaycastHit hit;
+
+            // Check to see if the mouse has been pressed and project a ray 0f 100 into the screen and store what it hits in hit
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100)) {
+
+                // Loop through all the agents and tell unity AI to move them to their destinations
+                foreach (GameObject a in agents) {
+
+                    a.GetComponent<AIControl>().agent.SetDestination(hit.point);
+                }
+            }
+        }
+    }
+}
