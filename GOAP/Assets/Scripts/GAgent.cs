2e0686fce1e947abd612e6f500642c220cb82ef4
Bhanu <uabhanu@gmail.com>
2021-10-30 15:42:53 +0100

    Mastered Bootcamp : AI For Beginners Goal Driven Behaviour : Goal Oriented Action Plan (GOAP) Agents Complete

--- GOAP/Assets/Scripts/GAgent.cs
+++ GOAP/Assets/Scripts/GAgent.cs
@@ -1,17 +1,42 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
+using System.Linq; //So we can access sorting functions
 using UnityEngine;
 
+public class SubGoal
+{
+    public bool bRemove;
+    public Dictionary<string , int> SubGoals;
+
+    public SubGoal(string s , int i , bool r)
+    {
+        SubGoals = new Dictionary<string , int>();
+        SubGoals.Add(s , i);
+        bRemove = r;
+    }
+}
 public class GAgent : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private GPlanner _gPlanner;
+    private Queue<GAction> _actionQueue;
+    private SubGoal _currentSubGoal;
+
+    [SerializeField] private GAction currentAction;
+    [SerializeField] private List<GAction> actionsList = new List<GAction>();
+    
+    public Dictionary<SubGoal , int> SubGoalsList = new Dictionary<SubGoal , int>();
+
+    private void Start()
     {
-        
+        GAction[] acts = GetComponents<GAction>();
+
+        foreach(GAction ga in acts)
+        {
+            actionsList.Add(ga);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void LateUpdate()
     {
         
     }
