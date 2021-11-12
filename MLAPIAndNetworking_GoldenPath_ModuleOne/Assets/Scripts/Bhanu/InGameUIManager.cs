1e51b7867d13e6119cc44e3e4ce4d02ab4a916d2
Bhanu <uabhanu@gmail.com>
2021-11-12 18:20:31 +0000

    MLAPI And Networking Basic Scene Ready and Functional Golden Path Module One Finished : Replaced OnGUI with my own UI using TMP and also added a few extra buttons that shut down the connection and go back to the Main Menu

---
+++ MLAPIAndNetworking_GoldenPath_ModuleOne/Assets/Scripts/Bhanu/InGameUIManager.cs
@@ -0,0 +1,105 @@
+using Unity.Netcode;
+using UnityEngine;
+
+namespace Bhanu
+{
+    public class InGameUIManager : MonoBehaviour
+    {
+        [SerializeField] private GameObject clientMenuObj;
+        [SerializeField] private GameObject hostMenuObj;
+        [SerializeField] private GameObject mainMenuObj;
+        [SerializeField] private GameObject serverMenuObj;
+
+        private void Start()
+        {
+            clientMenuObj.SetActive(false);
+            hostMenuObj.SetActive(false);
+            mainMenuObj.SetActive(false);
+            serverMenuObj.SetActive(false);
+            
+            if(!NetworkManager.Singleton.IsClient && !NetworkManager.Singleton.IsServer)
+            {
+                DisplayButtons();
+            }
+        }
+
+        private void DisplayButtons()
+        {
+            mainMenuObj.SetActive(true);
+        }
+
+        public void ClientButton()
+        {
+            clientMenuObj.SetActive(true);
+            hostMenuObj.SetActive(false);
+            mainMenuObj.SetActive(false);
+            serverMenuObj.SetActive(false);
+            
+            NetworkManager.Singleton.StartClient();
+        }
+
+        public void HostButton()
+        {
+            clientMenuObj.SetActive(false);
+            hostMenuObj.SetActive(true);
+            mainMenuObj.SetActive(false);
+            serverMenuObj.SetActive(false);
+            
+            NetworkManager.Singleton.StartHost();
+        }
+        
+        public void MoveButton()
+        {
+            NetworkObject playerObject = NetworkManager.Singleton.SpawnManager.GetLocalPlayerObject();
+            HelloWorldPlayer player = playerObject.GetComponent<HelloWorldPlayer>();
+            player.Move();
+        }
+        
+        public void RequestChangePositionButton()
+        {
+            NetworkObject playerObject = NetworkManager.Singleton.SpawnManager.GetLocalPlayerObject();
+            HelloWorldPlayer player = playerObject.GetComponent<HelloWorldPlayer>();
+            player.Move();
+        }
+
+        public void ServerButton()
+        {
+            clientMenuObj.SetActive(false);
+            hostMenuObj.SetActive(false);
+            mainMenuObj.SetActive(false);
+            serverMenuObj.SetActive(true);
+            
+            NetworkManager.Singleton.StartClient();
+        }
+
+        public void StopClientButton()
+        {
+            clientMenuObj.SetActive(false);
+            hostMenuObj.SetActive(false);
+            mainMenuObj.SetActive(true);
+            serverMenuObj.SetActive(false);
+            
+            NetworkManager.Singleton.Shutdown();
+        }
+        
+        public void StopHostButton()
+        {
+            clientMenuObj.SetActive(false);
+            hostMenuObj.SetActive(false);
+            mainMenuObj.SetActive(true);
+            serverMenuObj.SetActive(false);
+            
+            NetworkManager.Singleton.Shutdown();
+        }
+        
+        public void StopServerButton()
+        {
+            clientMenuObj.SetActive(false);
+            hostMenuObj.SetActive(false);
+            mainMenuObj.SetActive(true);
+            serverMenuObj.SetActive(false);
+            
+            NetworkManager.Singleton.Shutdown();
+        }
+    }
+}
