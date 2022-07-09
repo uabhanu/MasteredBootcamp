a34867f960734d269bb54f5269a0e9cda480b927
Bhanu <uabhanu@gmail.com>
2022-07-09 11:07:44 +0100

    Mastered Bootcamp : Sunney Valley Studios Implemented Object Pool and Destroy Helpers

---
+++ TopDownTanks/Assets/Scripts/DestroyHelper.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class DestroyHelper : MonoBehaviour
+{
+    public void DestroySelf()
+    {
+        Destroy(gameObject);
+    }
+}
--- TopDownTanks/Assets/Scripts/TankTurret.cs
+++ TopDownTanks/Assets/Scripts/TankTurret.cs
@@ -1,17 +1,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(ObjectPool))]
 public class TankTurret : MonoBehaviour
 {
     #region Variable Declarations
     
     private bool _bCanShoot;
     private  Collider2D[] _tankColliders2D;
+    private int _bulletPoolSize;
 
     [SerializeField] private float currentDelay = 0f;
     [SerializeField] private float reloadDelay = 1f;
     [SerializeField] private GameObject bulletPrefab;
     [SerializeField] private List<Transform> turretBarrelsList;
+    [SerializeField] private ObjectPool bulletPool;
     
     #endregion
 
@@ -19,7 +22,22 @@ public class TankTurret : MonoBehaviour
 
     private void Awake()
     {
-        _tankColliders2D = GetComponentsInParent<Collider2D>();
+        if(bulletPool == null)
+        {
+            bulletPool = GetComponent<ObjectPool>();
+        }
+        
+        _bulletPoolSize = bulletPool.PoolSize;
+
+        if(_tankColliders2D == null)
+        {
+            _tankColliders2D = GetComponentsInParent<Collider2D>();   
+        }
+    }
+
+    private void Start()
+    {
+        bulletPool.Initialize(bulletPrefab , _bulletPoolSize);
     }
 
     private void Update()
@@ -44,7 +62,8 @@ public class TankTurret : MonoBehaviour
 
             foreach (var barrel in turretBarrelsList)
             {
-                GameObject bulletObj = Instantiate(bulletPrefab);
+                //GameObject bulletObj = Instantiate(bulletPrefab);
+                GameObject bulletObj = bulletPool.CreateObject();
                 bulletObj.transform.position = barrel.position;
                 // Barrel Rotation 90 giving wrong result so left it as 0 ignoring the instructor's instruction
                 bulletObj.transform.localRotation = barrel.rotation;
