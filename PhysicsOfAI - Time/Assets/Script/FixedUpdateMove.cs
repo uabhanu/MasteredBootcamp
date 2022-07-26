8e2e806daafed67eb56e2962bcf808a6a204ee32
Bhanu <uabhanu@gmail.com>
2022-07-26 11:51:41 +0100

    MasteredBootcampGit : Removed unnecessary Projects from the Repo

--- PhysicsOfAI - Time/Assets/Script/FixedUpdateMove.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public class FixedUpdateMove : MonoBehaviour
-{
-    private void FixedUpdate()
-    {
-        transform.Translate(0 , 0 , Time.deltaTime); // Move the character in the Z direction, using 'time elapsed between updates' as amount.
-    }
-}
